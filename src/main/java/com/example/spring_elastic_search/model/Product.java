3a34aaa0bb7ed7f4c00370adbdb0b5f3aa639a63
saurabh352 <71172005+saurabh352@users.noreply.github.com>
2026-03-13 08:51:50 +0530

    commit 4

--- src/main/java/com/example/spring_elastic_search/model/Product.java
+++ src/main/java/com/example/spring_elastic_search/model/Product.java
@@ -1,14 +1,14 @@
 package com.example.spring_elastic_search.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Document(indexName = "products")
+@Getter
+@Setter
 public class Product {
     private int id;
     private String name;
